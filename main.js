ea31e5ccda0d15be75c10def71f6b6eadfc0fd79
Harish0411 <hariharanramasamy98@gmail.com>
2023-02-28 16:20:52 +0530

    create new array from user input

---
+++ main.js
@@ -0,0 +1,8 @@
+let len = Number(prompt("Enter length of an array"));
+let array = [];
+
+for(i=0; i<len; i++){
+let value = prompt("Enter the values");
+     array[i] = value;
+}
+console.log(array);
